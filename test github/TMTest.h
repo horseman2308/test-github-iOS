603a37840b61dbaee62af4f2909276ece1686408
Huy Nguyen <huynguyen@Huys-iMac.local>
2014-08-17 01:33:00 +0700

    added a test class

---
+++ test github/TMTest.h
@@ -0,0 +1,13 @@
+//
+//  TMTest.h
+//  test github
+//
+//  Created by Huy Nguyen on 8/17/14.
+//  Copyright (c) 2014 tinyman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TMTest : NSObject
+
+@end
---
+++ test github/TMTest.m
@@ -0,0 +1,13 @@
+//
+//  TMTest.m
+//  test github
+//
+//  Created by Huy Nguyen on 8/17/14.
+//  Copyright (c) 2014 tinyman. All rights reserved.
+//
+
+#import "TMTest.h"
+
+@implementation TMTest
+
+@end
